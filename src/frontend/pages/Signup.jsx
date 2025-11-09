9446971a4c610ae57b56cfff7508d538bad7a8b0
Santhosh Kumar 05 <santhoshkumarms@student.tce.edu>
2025-11-09 10:02:15 +0530

    commit

--- src/frontend/pages/Signup.jsx
+++ src/frontend/pages/Signup.jsx
@@ -15,7 +15,7 @@ const Signup = () => {
     setMessage("");
     
     try {
-      const res = await fetch("http://127.0.0.1:5000/auth/signup", {
+      const res = await fetch("https://agroscan-v9kw.onrender.com/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password })
